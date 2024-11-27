86e79483dd854bbc8bcbf8409a37ae5b7aa129f5
sinarhen <nicklchannel8@gmail.com>
2024-11-27 13:09:18 +0200

    Demo of the landing

--- clientSvelte/src/routes/+page.svelte
+++ clientSvelte/src/routes/+page.svelte
@@ -1,10 +1,11 @@
-<!-- Main.svelte -->
 <script lang="ts">
     import '../app.css';
     import { Input } from "@/components/ui/input";
     import { Button } from "@/components/ui/button";
     import { Label } from "@/components/ui/label";
     import * as RadioGroup from "$lib/components/ui/radio-group";
+    import {Check, Save, Share2} from "lucide-svelte";
+    import * as Card from "$lib/components/ui/card/index.js";
 
     let topic = "";
     const questions = [
@@ -18,7 +19,7 @@
                 { value: "None of the above", correct: true }
             ],
         },        {
-            type: "single",
+            type: "multiple",
             question: "What year did the first coalition collapse?",
             options: [
                 { value: "1803", correct: false },
@@ -62,21 +63,62 @@
     </Button>
 </div>
 
-<div class="w-full flex flex-col gap-y-10 mt-10">
-    {#each questions as question}
-        <div>
-            <h2 class="text-xl mb-5 font-medium">{question.question}</h2>
-            <RadioGroup.Root value="option-one">
-                {#each question.options as option}
-                    <div class="flex items-center space-x-2">
-                        <RadioGroup.Item value={option.value} id={option.value} />
-                        <Label for={option.value}>{option.value}</Label>
-                    </div>
-                {/each}
-            </RadioGroup.Root>
+<h2 class="font-bold text-2xl">Napoleonic wars</h2>
+<div class="w-full flex h-full relative xl:flex-row flex-col mt-6">
+    <section class="flex w-full flex-col gap-y-10">
+        {#each questions as question}
+            {#if question.type === "single"}
+                <div>
+                    <h3 class="text-xl mb-5 font-medium">{question.question}</h3>
+                    <RadioGroup.Root value="option-one">
+                        {#each question.options as option}
+                            <div class="flex items-center space-x-2">
+                                <RadioGroup.Item value={option.value} id={option.value} />
+                                <Label for={option.value}>{option.value}
+                                    {#if option.correct}
+                                        <span class="text-green">✅</span>
+                                    {/if}
+                                </Label>
+                            </div>
+                        {/each}
+                    </RadioGroup.Root>
+                </div>
+
+            {:else if question.type === "multiple"}
+                <div>
+                    Multiple choice question
+                </div>
+            {/if}
+        {/each}
+        <div class="flex w-full xl:justify-start justify-center">
+
+            <div class="grid w-full gap-y-2 xl:max-w-[400px] gap-x-2 grid-cols-4">
+                <Button class="col-span-4 gap-x-1">
+                    <Save size="16"/>
+                    Save
+                </Button>
+                <Button variant="outline" class="gap-x-1 inline-flex xl:hidden col-span-4">
+                    <Share2 size="16" />
+                    Share
+                </Button>
+
+            </div>
         </div>
-    {/each}
-    <Button class="mt-2">
-        Submit
-    </Button>
+    </section>
+    <section class="flex flex-col w-full">
+        <h5 class="text-lg font-semibold text-center">Export</h5>
+
+        <div class="grid grid-cols-2 mt-5 gap-2 w-full h-full items-center">
+            <Button variant="ghost" class="h-16 xl:h-full border enabled:hover:bg-blue-500 col-span-2 xl:col-span-1  rounded-sm flex items-center justify-center">
+                <h4 class="font-bold">Google Forms</h4>
+            </Button>
+            <Button variant="ghost" class=" h-16 xl:h-full border col-span-2 xl:col-span-1  rounded-sm flex items-center justify-center">
+                <h4 class="font-bold">Microsoft Forms</h4>
+            </Button>
+            <Button variant="ghost" class="h-16 xl:h-full border enabled:hover:bg-red-400 col-span-2 xl:col-span-2 rounded-sm flex items-center justify-center">
+                <h4 class="font-bold">PDF</h4>
+            </Button>
+
+        </div>
+    </section>
 </div>
