191fb4891e3ac8b8f87cee21774d61ea8486b9d6
sinarhen <nicklchannel8@gmail.com>
2024-11-27 13:14:34 +0200

    Responsiveness improvements

--- clientSvelte/src/routes/+page.svelte
+++ clientSvelte/src/routes/+page.svelte
@@ -105,10 +105,10 @@
             </div>
         </div>
     </section>
-    <section class="flex flex-col w-full">
+    <section class="flex flex-col w-full mt-14 xl:mt-0">
         <h5 class="text-lg font-semibold text-center">Export</h5>
 
-        <div class="grid grid-cols-2 mt-5 gap-2 w-full h-full items-center">
+        <div class="grid grid-cols-2 gap-2 w-full mt-4 xl:h-full xl:mt-0 items-center">
             <Button variant="ghost" class="h-16 xl:h-full border enabled:hover:bg-blue-500 col-span-2 xl:col-span-1  rounded-sm flex items-center justify-center">
                 <h4 class="font-bold">Google Forms</h4>
             </Button>
