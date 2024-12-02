5243abaf6349d5b33604127b2fd04bf072089132
sinarhen <nicklchannel8@gmail.com>
2024-12-02 22:32:55 +0200

    Popover added

--- clientSvelte/src/routes/components/GenerateQuestionButton.svelte
+++ clientSvelte/src/routes/components/GenerateQuestionButton.svelte
@@ -2,6 +2,8 @@
     import { Sparkles } from 'lucide-svelte';
     import { Button } from '@/components/ui/button';
     import * as Popover from "$lib/components/ui/popover";
+    import {Input} from "@/components/ui/input";
+    import {Label} from "@/components/ui/label";
 
     let isPopoverOpen = false;
     let topic = '';
@@ -17,19 +19,38 @@
 
 </script>
 
-<div class="relative">
+<div class="relative w-full">
     <Popover.Root>
-        <Popover.Trigger>Open</Popover.Trigger>
-        <Popover.Content>Place content for the popover here.</Popover.Content>
+        <Popover.Trigger class="w-full">
+            <Button
+                    size="sm"
+                    variant="outline"
+                    class="flex relative w-full rounded-l-none items-center justify-center gap-x-1"
+                    on:click={togglePopover}
+            >
+                <Sparkles size="16" />
+                Generate question
+            </Button>
+        </Popover.Trigger>
+        <Popover.Content side="top">
+            <Label>
+                Topic
+            </Label>
+            <p class="text-xs opacity-50">
+                Enter a topic to generate a question about
+            </p>
+            <Input class="mt-1.5"/>
+            <Button
+                    variant="ghost"
+                    size="sm"
+                    class="mt-2 gap-x-1"
+                    onclick={generateQuestion}
+            >
+                <Sparkles size="16"/>
+                Generate
+            </Button>
+        </Popover.Content>
     </Popover.Root>
-<!--    <Button-->
-<!--            size="sm"-->
-<!--            variant="outline"-->
-<!--            class="flex relative w-full rounded-l-none items-center justify-center gap-x-1"-->
-<!--            on:click={togglePopover}-->
-<!--    >-->
-<!--        <Sparkles size="16" />-->
-<!--        Generate question-->
-<!--    </Button>-->
+
 
 </div>
