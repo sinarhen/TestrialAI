83cfd3c2e0094d0177e7cea70d12aeaa9953a64b
sinarhen <nicklchannel8@gmail.com>
2024-12-12 20:18:07 +0200

    Design

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-    import './app.css';
+    import '../app.css';
     import Greeting from './components/Greeting.svelte';
-    import ExportSection from './components/ExportSection.svelte';
+    // import ExportSection from './components/ExportSection.svelte';
     import type { ActionData, PageServerData } from './$types';
     import Header from "./components/Header/Header.svelte";
     import QuestionList from "./components/QuestionList/QuestionList.svelte";
+    import {CircleHelp, Gauge, Timer} from "lucide-svelte";
 
     let topic: string = $state("Geography test");
     let { data, form }: { data: PageServerData, form: ActionData } = $props();
@@ -19,7 +20,14 @@
 <Greeting bind:topic userName={data.user?.username} onCreateSurvey={onCreateSurvey} />
 
 <h2 class="font-bold text-2xl">Geography test</h2>
+<div class="text-sm flex mt-1 gap-x-4">
+    <span class="gap-x-1 flex items-center"><CircleHelp size="12"/> 8 Questions</span>
+    <span class="gap-x-1 flex items-center"><Timer size="12"/> 10 Minutes</span>
+    <span class="gap-x-1 flex items-center"><Gauge size="12"/> Hard</span>
+
+
+</div>
 <div class="w-full flex h-full gap-x-12 relative xl:flex-row flex-col mt-6">
     <QuestionList />
-    <ExportSection />
+<!--    <ExportSection />-->
 </div>
