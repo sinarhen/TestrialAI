a89417032456af5794023a02dd051d66f18fb780
sinarhen <nicklchannel8@gmail.com>
2025-01-12 01:38:15 +0200

    Everything renamed to test

---
+++ app/src/routes/(client)/test/[id]/components/TestDetails.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import { Grip } from 'lucide-svelte';
+	import TestDetailsQuestion from './TestDetailsQuestion.svelte';
+	import type { QuestionState, TestState } from '../types';
+	import type { Question } from '@/types/entities';
+
+	let draggedIndex: number | null = $state<number | null>(null);
+
+	const {
+		initialQuestionsCount,
+		test = $bindable()
+	}: {
+		test: TestState;
+		initialQuestionsCount: number;
+	} = $props();
+
+	function onDragStart(event: DragEvent, index: number) {
+		draggedIndex = index;
+		const gripElement = event.currentTarget as HTMLElement;
+		const dragImageElement = gripElement.parentNode as HTMLElement;
+		const rect = dragImageElement.getBoundingClientRect();
+		const offsetX = event.clientX - rect.left;
+		const offsetY = event.clientY - rect.top;
+		event.dataTransfer?.setDragImage(dragImageElement, offsetX, offsetY);
+	}
+
+	function onDragOver(event: DragEvent) {
+		event.preventDefault();
+	}
+
+	function onDrop(event: DragEvent, targetIndex: number) {
+		event.preventDefault();
+		if (draggedIndex !== null && draggedIndex !== targetIndex) {
+			const qs = test.questions;
+			const newQs = [...(qs ?? [])];
+			const [draggedItem] = newQs.splice(draggedIndex, 1);
+			newQs.splice(targetIndex, 0, draggedItem);
+			test.questions = newQs;
+		}
+		draggedIndex = null;
+	}
+	const deleteQuestionInStore = (index: number) => test.questions.splice(index, 1);
+
+	const updateQuestionInStore = (index: number, newQuestion: QuestionState) =>
+		test.questions.splice(index, 1, newQuestion);
+
+	// That means the question is new because its index is greater than the length of the server side state
+	const isQuestionNewlyAdded = (index: number): boolean => index >= initialQuestionsCount;
+</script>
+
+<section class="relative mt-8 flex h-full w-full flex-col">
+	<div class="flex w-full flex-col gap-y-12">
+		{#each test.questions ?? [] as question, index (index)}
+			<div
+				class={`motion-opacity-in-0 -motion-translate-y-in-25 motion-delay-[--delay] group relative`}
+				style={`--delay: ${!isQuestionNewlyAdded(index) ? 200 + 150 * (index + 1) : 0}ms`}
+				ondragover={onDragOver}
+				ondrop={(event) => onDrop(event, index)}
+				role="list"
+			>
+				{#if question.status !== 'generated' && question.status !== 'generating'}
+					<div
+						role="listitem"
+						class="absolute -left-14 flex h-full cursor-grab items-center opacity-10 transition-opacity active:cursor-grabbing active:hover:opacity-50 group-hover:opacity-25 xl:-left-20"
+						draggable="true"
+						ondragstart={(event) => onDragStart(event, index)}
+					>
+						<Grip size="32" />
+					</div>
+				{/if}
+				<TestDetailsQuestion
+					{question}
+					updateQuestionInStore={(newQuestion) => updateQuestionInStore(index, newQuestion)}
+					testId={test.id}
+					deleteQuestionInStore={() => deleteQuestionInStore(index)}
+				/>
+			</div>
+		{/each}
+	</div>
+</section>
---
+++ app/src/routes/(client)/test/[id]/components/TestDetailsQuestion.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import { Check, Sparkles, PlusCircle, Loader } from 'lucide-svelte';
+	import { questionState, type QuestionState } from '../types';
+	import GeneratedQuestionActions from './(components)/TestDetailsQuestion/GeneratedQuestionActions.svelte';
+	import QuestionTitle from './(components)/TestDetailsQuestion/Header/QuestionTitle.svelte';
+	import QuestionAnswers from './(components)/TestDetailsQuestion/QuestionAnswers.svelte';
+	import QuestionAIEdit from './(components)/TestDetailsQuestion/Header/Tools/QuestionAIEdit.svelte';
+	import QuestionDelete from './(components)/TestDetailsQuestion/Header/Tools/QuestionDelete.svelte';
+	import QuestionEdit from './(components)/TestDetailsQuestion/Header/Tools/QuestionEdit.svelte';
+
+	const {
+		testId,
+		question,
+		updateQuestionInStore,
+		deleteQuestionInStore
+	}: {
+		testId: string;
+		question: QuestionState;
+		updateQuestionInStore: (updatedQuestion: QuestionState) => void;
+		deleteQuestionInStore: () => void;
+	} = $props();
+
+	const isJustGenerated = $derived(
+		questionState.isSaved(question) ? question.isJustGenerated : false
+	);
+</script>
+
+<div class="group" class:motion-preset-confetti={isJustGenerated}>
+	{#key question.status}
+		<span class="motion-preset-focus inline-flex items-center gap-x-1 text-xs opacity-50">
+			<div>
+				{#if questionState.isGenerating(question)}
+					<Loader class="motion-preset-spin" size="12" />
+				{:else if questionState.isGenerated(question)}
+					<Sparkles size="12" />
+				{:else if questionState.isNew(question)}
+					<PlusCircle size="12" />
+				{:else if questionState.isSaved(question)}
+					<Check size="12" />
+				{:else}
+					<Check size="12" />
+				{/if}
+			</div>
+
+			{question.status.charAt(0).toUpperCase() + question.status.slice(1)}
+		</span>
+	{/key}
+
+	<header class="mb-5 mt-0.5 inline-flex w-full justify-between gap-x-7">
+		<QuestionTitle questionTitle={question.question} />
+
+		{#if questionState.isEditable(question)}
+			<div class="flex h-full items-center gap-x-2">
+				{#if questionState.isExisting(question)}
+					<QuestionAIEdit {updateQuestionInStore} {question} {testId} />
+				{/if}
+				<QuestionEdit {testId} {question} {updateQuestionInStore} />
+				<QuestionDelete {question} {testId} {deleteQuestionInStore} />
+			</div>
+		{/if}
+	</header>
+
+	<QuestionAnswers {question} />
+
+	{#if questionState.isGenerated(question)}
+		<GeneratedQuestionActions {testId} {question} {updateQuestionInStore} {deleteQuestionInStore} />
+	{/if}
+</div>
