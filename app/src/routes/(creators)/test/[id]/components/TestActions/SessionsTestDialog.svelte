0b46131c083a4b73f7737a9515455b085f2cf531
sinarhen <nicklchannel8@gmail.com>
2025-01-17 21:40:17 +0200

    feat: enhance session display in SessionsTestDialog with copy functionality and improved layout

--- app/src/routes/(creators)/test/[id]/components/TestActions/SessionsTestDialog.svelte
+++ app/src/routes/(creators)/test/[id]/components/TestActions/SessionsTestDialog.svelte
@@ -4,8 +4,11 @@
 	import {
 		ArrowUpRight,
 		Circle,
+		Copy,
 		ExternalLink,
 		FileClock,
+		Play,
+		QrCode,
 		Settings,
 		Trash,
 		User
@@ -13,6 +16,10 @@
 	import * as Dialog from '@/components/ui/dialog';
 	import * as Accordion from '@/components/ui/accordion';
 	import Separator from '@/components/ui/separator/separator.svelte';
+	import { Input } from '@/components/ui/input';
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+	import { copy } from '@/utils/copy';
 
 	const {
 		sessions
@@ -45,31 +52,42 @@
 		{#if !!sessions?.length}
 			<div class="flex flex-col gap-y-2">
 				{#each sessions as session}
-					<Button variant="outline" class="block h-full w-full rounded-md border p-4">
-						<div class="flex justify-between">
-							<div class="flex w-full justify-between">
-								<div>
-									<span class="flex text-xs opacity-75">
-										{session.startTime.getDate()}/{session.startTime.getMonth() +
-											1}/{session.startTime.getFullYear()}
-									</span>
-									<h2 class="flex w-full gap-x-1 text-start text-sm font-medium">
-										{session.testStateJson.title}
-									</h2>
-								</div>
-								<div class="flex h-full gap-x-3">
-									<span class="flex items-center gap-x-1 text-xs">
-										<User size="12" />
-										{session.participants.length}
-									</span>
-									<span class="flex items-center gap-x-1 text-xs">
-										{!session.endTime ? 'Active' : 'Finished'}
-										<Circle fill={!session.endTime ? 'green' : 'red'} size="12" />
-									</span>
-								</div>
+					<div class="block h-full w-full rounded-md border p-4">
+						<div class="flex w-full items-end justify-between">
+							<div>
+								<h2 class="mt-1 flex w-full gap-x-1 text-start text-sm font-medium">
+									{session.testStateJson.title}
+								</h2>
+							</div>
+							<div class="flex items-center gap-x-1 text-lg uppercase">
+								{session.code}
+								<Button variant="ghost" class="size-7" size="icon">
+									<Copy size="14" />
+								</Button>
 							</div>
 						</div>
-					</Button>
+						<Separator class="my-3" />
+						<div class="flex justify-between">
+							<div class="flex gap-x-3 text-sm">
+								<span class="flex items-center gap-x-1">
+									<Circle fill={!session.endTime ? 'green' : 'red'} size="12" />
+									{!session.endTime ? 'Active' : 'Finished'}
+								</span>
+								<span class="flex items-center gap-x-1">
+									<User strokeWidth={2.5} size="12" />
+									{session.participants.length}
+								</span>
+							</div>
+
+							<div class="flex justify-end gap-x-1">
+								<Button class="h-8 px-3 text-xs" variant="outline" size="sm">Details</Button>
+
+								<Button class="h-8 px-3 text-xs" variant="outline" size="sm">Configure</Button>
+
+								<Button class="h-8 px-3 text-xs" size="sm">End</Button>
+							</div>
+						</div>
+					</div>
 				{/each}
 			</div>
 		{/if}
