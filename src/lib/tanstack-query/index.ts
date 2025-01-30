79b961bce25b0e2637107e5c8d2238a2a727bbdb
sinarhen <nicklchannel8@gmail.com>
2025-01-30 23:14:02 +0200

    refactor: remove unused modules and consolidate API client initialization; streamline codebase for improved maintainability

--- src/lib/tanstack-query/index.ts
+++ src/lib/tanstack-query/index.ts
@@ -1,15 +1,22 @@
+// import type { ClientRequestOptions } from 'hono';
+// import { IamModule } from './domains/iam';
+// import { UsersModule } from './domains/users';
+// import { RequestOptions } from './request-options';
+// import { TestsModule } from './domains/tests';
+// import { QuestionsModule } from './domains/questions';
+
+// class TanstackQueryModule extends RequestOptions {
+// 	iam = new IamModule(this.opts);
+// 	users = new UsersModule(this.opts);
+// 	tests = new TestsModule(this.opts);
+// 	questions = new QuestionsModule(this.opts);
+// }
+
+// export const api = (opts?: ClientRequestOptions) => new TanstackQueryModule(opts);
+
+// WITHOUT Tanstack
+
 import type { ClientRequestOptions } from 'hono';
-import { IamModule } from './domains/iam';
-import { UsersModule } from './domains/users';
-import { RequestOptions } from './request-options';
-import { TestsModule } from './domains/tests';
-import { QuestionsModule } from './domains/questions';
+import { honoClient } from '@/utils/api';
 
-class TanstackQueryModule extends RequestOptions {
-	iam = new IamModule(this.opts);
-	users = new UsersModule(this.opts);
-	tests = new TestsModule(this.opts);
-	questions = new QuestionsModule(this.opts);
-}
-
-export const api = (opts?: ClientRequestOptions) => new TanstackQueryModule(opts);
+export const api = (opts?: ClientRequestOptions) => honoClient(opts);
