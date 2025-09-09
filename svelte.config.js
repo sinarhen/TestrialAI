c9c1ef3e3276f79504926998b9d61337d62fad23
sinarhen <nicklchannel8@gmail.com>
2025-09-09 23:09:08 +0300

    feat: Update project configuration and dependencies
    - Added support for the Svelte adapter for Node.js in package.json and package-lock.json.
    - Introduced a new railway.toml file for deployment configuration.
    - Updated svelte.config.js to use the Node.js adapter and specified output directory.
    - Enhanced hooks.server.ts to include reflect-metadata for improved functionality.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import 'reflect-metadata';
 
@@ -13,8 +13,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			runtime: 'nodejs18.x',
-			maxDuration: 30
+			out: 'build'
 		}),
 		alias: {
 			'@/*': './src/lib/*',
